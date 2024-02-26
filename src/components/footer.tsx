e6acb08106511c11a6a0986ee631c14a2ee73303
Jean Martins <jeanmrtns4@gmail.com>
2024-02-26 12:43:02 -0300

    feat: add cmdk

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="flex items-center py-4 px-2">
+      <span>Jean Martins &copy; {new Date().getFullYear()}</span>
+    </footer>
+  )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { HeaderLink } from './header-link'
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between px-6 py-3">
+    <header className="flex items-center justify-between px-6 py-3 justify-self-start self-start w-full">
       <h1>Jean Martins</h1>
 
       <nav className="flex items-center gap-8 uppercase">
