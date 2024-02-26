67de69c3b01f48c84609c0745f141dd64d85df37
Jean Martins <jeanmrtns4@gmail.com>
2024-02-26 12:47:53 -0300

    feat: create pages files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HomeCmdk } from '@/components/home-cmdk'
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="relative flex-1 flex items-center justify-center flex-col">
       <div className="mb-8 flex flex-col gap-4">
         <h1 className="text-5xl font-bold">Jean Martins</h1>
         <span className="text-sm">Full-Stack Developer @Pluginbot</span>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
 import { ToggleThemeButton } from './toggle-theme'
 import { HeaderLink } from './header-link'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="flex items-center justify-between px-6 py-3 justify-self-start self-start w-full">
-      <h1>Jean Martins</h1>
+      <Link href="/">Jean Martins</Link>
 
       <nav className="flex items-center gap-8 uppercase">
         <HeaderLink title="About" href="/about" />
