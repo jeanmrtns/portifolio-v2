5edac48b80c844464397a92e05689c2217c11e42
Jean Martins <jeanmrtns4@gmail.com>
2024-02-26 12:02:43 -0300

    feat: create header

---
+++ src/components/header-link.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+interface HeaderLinkProps {
+  title: string
+  href: string
+}
+
+export function HeaderLink({ title, href }: HeaderLinkProps) {
+  return (
+    <Link
+      className="text-sm text-ring hover:text-primary transition-colors"
+      href={href}
+    >
+      {title}
+    </Link>
+  )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+import { ToggleThemeButton } from './toggle-theme'
+import { HeaderLink } from './header-link'
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-between px-6 py-3">
+      <h1>Jean Martins</h1>
+
+      <nav className="flex items-center gap-8 uppercase">
+        <HeaderLink title="About" href="/about" />
+        <HeaderLink title="Projects" href="/projects" />
+        <HeaderLink title="Career" href="/career" />
+      </nav>
+
+      <div>
+        <ToggleThemeButton />
+      </div>
+    </header>
+  )
+}
