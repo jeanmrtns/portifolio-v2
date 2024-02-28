c2b7f9c8b204018fb121da760f200ba7dc03739f
Jean Martins <jeanmrtns4@gmail.com>
2024-02-28 16:16:09 -0300

    feat: change animations for responsivity

--- src/components/background-circles.tsx
+++ src/components/background-circles.tsx
@@ -3,12 +3,12 @@ import { motion } from 'framer-motion'
 export function BackgroundCircles() {
   return (
     <motion.div
-      className="relative flex justify-center items-center"
+      className="relative hidden lg:flex justify-center items-center"
       initial={{
         opacity: 0,
       }}
       animate={{
-        scale: [1, 2, 2, 3, 1],
+        scale: [1, 1, 1, 1, 1],
         opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
         borderRadius: ['20%', '20%', '50%', '80%', '20%'],
       }}
@@ -16,11 +16,11 @@ export function BackgroundCircles() {
         duration: 2.5,
       }}
     >
-      <div className="absolute border dark:border-zinc-800 rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="absolute border dark:border-zinc-800 rounded-full h-[300px] w-[300px] mt-52 animate-ping" />
-      <div className="absolute border dark:border-zinc-800 rounded-full h-[500px] w-[500px] mt-52 animate-ping" />
-      <div className="absolute border rounded-full border-red-600 opacity-20  animate-pulse h-[400px] w-[400px] mt-52" />
-      <div className="absolute border rounded-full border-zinc-800 opacity-20  animate-pulse h-[500px] w-[500px] mt-52" />
+      <div className="absolute border dark:border-zinc-800 rounded-full h-[200px] w-[200px] mt-20 animate-ping" />
+      <div className="absolute border dark:border-zinc-800 rounded-full h-[300px] w-[300px] mt-20 animate-ping" />
+      <div className="absolute border dark:border-zinc-800 rounded-full h-[500px] w-[500px] mt-20 animate-ping" />
+      <div className="absolute border rounded-full border-red-600 opacity-20  animate-pulse h-[400px] w-[400px] mt-20" />
+      <div className="absolute border rounded-full border-zinc-800 opacity-20  animate-pulse h-[500px] w-[500px] mt-20" />
     </motion.div>
   )
 }
