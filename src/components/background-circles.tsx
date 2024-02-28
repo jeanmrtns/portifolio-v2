9caea14151e53f124b4b6040af319f774dbb3209
Jean Martins <jeanmrtns4@gmail.com>
2024-02-28 16:11:43 -0300

    style: change circle animation size

--- src/components/background-circles.tsx
+++ src/components/background-circles.tsx
@@ -19,8 +19,8 @@ export function BackgroundCircles() {
       <div className="absolute border dark:border-zinc-800 rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
       <div className="absolute border dark:border-zinc-800 rounded-full h-[300px] w-[300px] mt-52 animate-ping" />
       <div className="absolute border dark:border-zinc-800 rounded-full h-[500px] w-[500px] mt-52 animate-ping" />
-      <div className="absolute border rounded-full border-red-600 opacity-20  animate-pulse h-[650px] w-[650px] mt-52" />
-      <div className="absolute border rounded-full border-zinc-800 opacity-20  animate-pulse h-[800px] w-[800px] mt-52" />
+      <div className="absolute border rounded-full border-red-600 opacity-20  animate-pulse h-[400px] w-[400px] mt-52" />
+      <div className="absolute border rounded-full border-zinc-800 opacity-20  animate-pulse h-[500px] w-[500px] mt-52" />
     </motion.div>
   )
 }
